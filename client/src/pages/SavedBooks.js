f65535e567daf763c715279d85f9bd2b339fc76c
kg-phantom <ksurbayan@gmail.com>
2022-01-08 12:30:55 -0800

    execute LOGIN_USER and CREATE_USER mutations

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 import { useQuery, useMutation } from '@apollo/client';
 
-// import { deleteBook } from '../utils/API';
 import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
